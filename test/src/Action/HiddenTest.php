a1332d6dceecd74b3f92e3b74933a07340ec6da2
Bitnix <bitnix@riseup.net>
2020-10-17 12:39:19 +0100

    Added Hidden

---
+++ test/src/Action/HiddenTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+namespace Bitnix\Form\Action;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ...
+ *
+ * @version 0.1.0
+ */
+class HiddenTest extends TestCase {
+
+    public function testConstructor() {
+
+        $input = new Hidden('foo', [
+            'value' => '...',
+            'label' => 'Foo',
+            'usage' => 'Foo usage...'
+        ]);
+
+        $this->assertEquals('hidden', $input->attribute('type'));
+        $this->assertEquals('foo', $input->attribute('name'));
+        $this->assertEquals('...', $input->attribute('value'));
+
+        $this->assertEquals('Foo', $input->label());
+        $this->assertEquals('Foo usage...', $input->usage());
+    }
+
+}
