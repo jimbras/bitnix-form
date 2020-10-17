9e9357446210c65875bd39e2a610349472c7cc79
Bitnix <bitnix@riseup.net>
2020-10-17 11:36:16 +0100

    Added Checkbox

---
+++ test/src/Action/CheckboxTest.php
@@ -0,0 +1,116 @@
+<?php declare(strict_types=1);
+
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+namespace Bitnix\Form\Action;
+
+use Bitnix\Form\Sanitizer,
+    PHPUnit\Framework\TestCase;
+
+/**
+ * @version 0.1.0
+ */
+class CheckboxTest extends TestCase {
+
+    public function testConstructor() {
+        $input = new Checkbox('foo', [
+            'value' => 'bar',
+            'label' => 'Foo',
+            'usage' => 'Foo usage...'
+        ]);
+
+        $this->assertEquals('checkbox', $input->attribute('type'));
+        $this->assertEquals('foo', $input->attribute('name'));
+        $this->assertEquals('bar', $input->attribute('value'));
+        $this->assertFalse($input->attribute('checked'));
+        $this->assertFalse($input->attribute('indeterminate'));
+        $this->assertEquals('Foo', $input->label());
+        $this->assertEquals('Foo usage...', $input->usage());
+
+        $input = new Checkbox('foo', [
+            'value' => 'bar',
+            'checked' => true,
+            'indeterminate' => 'whatever...'
+        ]);
+
+        $this->assertFalse($input->attribute('checked'));
+        $this->assertTrue($input->attribute('indeterminate'));
+
+        $input = new Checkbox('foo', [
+            'value' => 'bar',
+            'checked' => true
+        ]);
+
+        $this->assertTrue($input->attribute('checked'));
+        $this->assertFalse($input->attribute('indeterminate'));
+    }
+
+    public function testUpdateNotSubmitted() {
+        $validator = $this->createMock(Sanitizer::CLASS);
+        $validator
+            ->expects($this->once())
+            ->method('filter')
+            ->with('foo', null)
+            ->will($this->returnValue(null));
+        $validator
+            ->expects($this->once())
+            ->method('validate')
+            ->with('foo', null)
+            ->will($this->returnValue([]));
+
+        $input = new Checkbox('foo', ['value' => 'bar', 'checked' => true]);
+
+        $html = $input->render(['checked' => false]);
+        $this->assertStringContainsString('checked', $html);
+
+        $this->assertTrue($input->attribute('checked'));
+        $this->assertNull($input->process($validator, []));
+        $this->assertFalse($input->attribute('checked'));
+        $this->assertFalse($input->attribute('indeterminate'));
+
+        $html = $input->render(['checked' => true]);
+        $this->assertStringNotContainsString('checked', $html);
+    }
+
+    public function testUpdateSubmitted() {
+        $validator = $this->createMock(Sanitizer::CLASS);
+        $validator
+            ->expects($this->once())
+            ->method('filter')
+            ->with('foo', 'bar')
+            ->will($this->returnValue('bar'));
+        $validator
+            ->expects($this->once())
+            ->method('validate')
+            ->with('foo', 'bar')
+            ->will($this->returnValue([]));
+
+        $input = new Checkbox('foo', ['value' => 'bar', 'indeterminate' => true]);
+
+        $html = $input->render(['checked' => true]);
+        $this->assertStringNotContainsString('checked', $html);
+
+        $this->assertFalse($input->attribute('checked'));
+        $this->assertTrue($input->attribute('indeterminate'));
+        $this->assertEquals('bar', $input->process($validator, ['foo' => 'bar']));
+        $this->assertTrue($input->attribute('checked'));
+        $this->assertFalse($input->attribute('indeterminate'));
+
+        $html = $input->render(['checked' => false]);
+        $this->assertStringContainsString('checked', $html);
+    }
+
+}
